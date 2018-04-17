c28c5f3d0352769eaad3e852d428d032add8c836
Vanja Petreski <vpetreski@gmail.com>
2018-04-17 16:58:11 -0500

    GitGub Ribbon; README updated; CloudFront cache invalidation;

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 npm run build && aws s3 sync build/ s3://petreski-react-countries
+
+// https://aws.amazon.com/cloudfront/pricing
+aws cloudfront create-invalidation --distribution-id=E2F18KNA2IIS8J --paths /
