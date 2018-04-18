ac07a6d0a638ac7365652f37414617c0e97b48b1
Vanja Petreski <vpetreski@gmail.com>
2018-04-18 18:57:45 -0500

    Rotating Earth

--- src/components/ReactCountries.js
+++ src/components/ReactCountries.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import './ReactCountries.css';
 import Button from 'material-ui/Button';
+import './RotatingEarth';
+import RotatingEarth from "./RotatingEarth";
 
 class ReactCountries extends Component {
     render() {
         return (
             <div className="RC">
-                <Button variant="raised" color="primary">
-                    Material UI
-                </Button>
+                {/* TODO - move as component? */}
+                <div className="RC-toolbar">
+                    <Button variant="raised" color="primary">
+                        Add Country
+                    </Button>
+                    <RotatingEarth/>
+                </div>
             </div>
         );
     }
