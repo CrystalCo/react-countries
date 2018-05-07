49269765525a765a11f395c49f8f278f5274924c
Vanja Petreski <vpetreski@gmail.com>
2018-05-07 13:55:41 -0500

    Loading indicator component.

--- src/components/RcList.js
+++ src/components/RcList.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import List from 'material-ui/List';
 import RcRow from "./RcRow";
+import Loading from "./Loading";
 
 export default function RcList({countries, onCountryVisitedChange, onCountryDeleted}) {
     return (
         <Paper elevation={1}>
             <List className="RC-list">
+                {/*<Loading />*/}
                 {countries.map(country =>
                     <RcRow key={country.code} {...country} onCountryVisitedChange={onCountryVisitedChange} onCountryDeleted={onCountryDeleted}/>
                 )}
