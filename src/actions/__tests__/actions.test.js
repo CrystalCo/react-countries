edb24bbbd9f5391b3240c3226f03a4176eeebeb0
Vanja Petreski <vpetreski@gmail.com>
2018-06-05 15:20:17 -0500

    Cleaning up..

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -34,33 +34,6 @@ const allCountriesMockData = [{
         "57"
     ],
     "capital": "Bogotá",
-    "altSpellings": [
-        "CO",
-        "Republic of Colombia",
-        "República de Colombia"
-    ],
-    "region": "Americas",
-    "subregion": "South America",
-    "population": 48759958,
-    "latlng": [
-        4,
-        -72
-    ],
-    "demonym": "Colombian",
-    "area": 1141748,
-    "gini": 55.9,
-    "timezones": [
-        "UTC-05:00"
-    ],
-    "borders": [
-        "BRA",
-        "ECU",
-        "PAN",
-        "PER",
-        "VEN"
-    ],
-    "nativeName": "Colombia",
-    "numericCode": "170",
     "currencies": [
         {
             "code": "COP",
@@ -76,45 +49,6 @@ const allCountriesMockData = [{
             "nativeName": "Español"
         }
     ],
-    "translations": {
-        "de": "Kolumbien",
-        "es": "Colombia",
-        "fr": "Colombie",
-        "ja": "コロンビア",
-        "it": "Colombia",
-        "br": "Colômbia",
-        "pt": "Colômbia",
-        "nl": "Colombia",
-        "hr": "Kolumbija",
-        "fa": "کلمبیا"
-    },
-    "flag": "https://restcountries.eu/data/col.svg",
-    "regionalBlocs": [
-        {
-            "acronym": "PA",
-            "name": "Pacific Alliance",
-            "otherAcronyms": [],
-            "otherNames": [
-                "Alianza del Pacífico"
-            ]
-        },
-        {
-            "acronym": "USAN",
-            "name": "Union of South American Nations",
-            "otherAcronyms": [
-                "UNASUR",
-                "UNASUL",
-                "UZAN"
-            ],
-            "otherNames": [
-                "Unión de Naciones Suramericanas",
-                "União de Nações Sul-Americanas",
-                "Unie van Zuid-Amerikaanse Naties",
-                "South American Union"
-            ]
-        }
-    ],
-    "cioc": "COL"
 }];
 
 beforeEach(() => {
